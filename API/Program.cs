9630d5e8c63ff8f7b0da07a64baee2924dfaaf3c
Amrut Kumar <amrutkumar165@gmail.com>
2023-06-20 02:31:39 +0530

    Added Identity, Authentication and Authorization

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using API.Middleware;
 using API.Errors;
 using API.Extensions;
+using Core.Entities.Identity;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 
@@ -27,6 +31,7 @@ app.UseStaticFiles();
 
 app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -34,11 +39,15 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
 var logger = services.GetRequiredService<ILogger<Program>>();
 try
 {
     await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
+    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
 }
 catch (Exception ex)
 {
---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,28 @@
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager){
+            if(!userManager.Users.Any()){
+                var user = new AppUser{
+                    DisplayName = "Sonu",
+                    Email = "sonu_cakebake@gmail.com",
+                    UserName = "sonu_admin",
+                    Address = new Address{
+                        FirstName = "Sonu",
+                        LastName = "Skyfire",
+                        Street = "Ambedkar Road",
+                        City = "Bengaluru",
+                        State = "Karnataka",
+                        ZipCode = "560066"
+                    }
+                };
+
+                await userManager.CreateAsync(user, "Sonu@312");
+            }
+        }
+    }
+}
