3dc1036b315992080ef31674716bf808ec41a50a
Amrut Kumar <amrutkumar165@gmail.com>
2023-06-20 22:48:58 +0530

    Added client identity, validation and guards

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -11,12 +11,15 @@ import { BusyService } from '../services/busy.service';
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
 
-  constructor(private busyService: BusyService) {}
+  constructor(private busyService: BusyService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    this.busyService.busy();
+    if (!request.url.includes('emailExists')) {
+      this.busyService.busy();
+    }
+
     return next.handle(request).pipe(
-      delay(1000),
+      delay(800),
       finalize(() => this.busyService.idle())
     );
   }
